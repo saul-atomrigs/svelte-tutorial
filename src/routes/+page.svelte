af0fbaff2d785d91f963c96768e1d5853eba7ae3
Saul Lee <solleedata@gmail.com>
2023-10-04 14:09:43 +0900

    tutorial (Intro)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Nested from '$lib/components/ui/Nested.svelte';
+
+	let name = 'Svelte';
+	const src = 'https://via.placeholder.com/150';
+
+	let string = 'this string contains some <strong>HTML</strong>';
+</script>
+
+<h1>Hello {name.toUpperCase()}</h1>
+
+<p>This is an example</p>
+
+<img {src} alt="placeholder" />
+
+<Nested />
+
+<p>{@html string}</p>
+
+<style>
+	p {
+		color: red;
+	}
+</style>
