21d42b8eaadacb0b6d8fb326d7992659fcf90e6f
Saul Lee <solleedata@gmail.com>
2023-10-04 14:33:24 +0900

    tutorial (Reactivity)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,27 @@
 	const src = 'https://via.placeholder.com/150';
 
 	let string = 'this string contains some <strong>HTML</strong>';
+
+	let count = 0;
+	$: doubled = count * 2;
+	function increment() {
+		count++;
+	}
+
+	$: {
+		console.log(`the count is ${count}`);
+		console.log(`the doubled count is ${doubled}`);
+	}
+
+	$: if (count > 5) {
+		alert('count is greater than 5');
+	}
+
+	let numbers = [1, 2, 3, 4];
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1];
+	}
+	$: sum = numbers.reduce((a, b) => a + b, 0);
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
@@ -16,6 +37,16 @@
 <Nested />
 
 <p>{@html string}</p>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+<button on:click={addNumber}> Add a number </button>
 
 <style>
 	p {
