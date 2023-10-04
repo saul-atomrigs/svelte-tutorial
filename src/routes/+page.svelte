08287ed3315fd5440309c4abdfadbd4394a738f1
Saul Lee <solleedata@gmail.com>
2023-10-04 14:56:52 +0900

    Tutorial (Logic)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Nested from '$lib/components/ui/Nested.svelte';
+	import { getRandomNumber } from '../lib/utils.js';
 
 	let name = 'Svelte';
 	const src = 'https://via.placeholder.com/150';
@@ -26,6 +27,15 @@
 		numbers = [...numbers, numbers.length + 1];
 	}
 	$: sum = numbers.reduce((a, b) => a + b, 0);
+
+	const colors = ['red', 'green', 'blue'];
+	let selected = colors[0];
+
+	let promise = getRandomNumber();
+
+	function handleClick() {
+		promise = getRandomNumber();
+	}
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
@@ -44,10 +54,38 @@
 	{count === 1 ? 'time' : 'times'}
 </button>
 
+{#if count > 5}
+	<p>count is greater than 5</p>
+{:else}
+	<p>count is not greater than 5</p>
+{/if}
+
+<div>
+	{#each colors as color, i}
+		<button
+			aria-current={selected === { color }}
+			aria-label={color}
+			style="background:{color}"
+			on:click={() => (selected = { color })}
+		>
+			{i + 1}
+		</button>
+	{/each}
+</div>
 <p>{count} doubled is {doubled}</p>
 
 <p>{numbers.join(' + ')} = {sum}</p>
 <button on:click={addNumber}> Add a number </button>
+
+<button on:click={handleClick}> Get Random Number </button>
+
+{#await promise}
+	<p>waiting...</p>
+{:then number}
+	<p>{number}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
 
 <style>
 	p {
